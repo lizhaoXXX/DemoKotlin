319cf4df68b373f1123acd03fb44559f5108e62d
Your NamelizhaoXXX <li939858718@qq.com>
2017-08-24 15:23:25 +0800

    Initial commit

---
+++ app/src/main/java/popup/demo/com/org/demokotlin/KotlinActivity.kt
@@ -0,0 +1,26 @@
+package popup.demo.com.org.demokotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+
+        println("sum: ${sum(10,5)}")
+        println("sum: ${sum03(10,"ABC")}")
+    }
+
+    /**
+     * 返回的值的函数
+     */
+    fun sum(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    fun sum02(a: Int, b: Int) = a * b;
+
+    fun sum03(a: Int, b: String) = b + a;
+}
